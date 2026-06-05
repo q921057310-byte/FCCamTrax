c2085b53badb0d0b6038d289bbc0ff685a1d30ac
xingxing <q921057310@gmail.com>
2026-06-05 21:37:17 +0800

    翻译

---
+++ translations/FCCamTrax_fr.ts
@@ -0,0 +1,307 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+  <context>
+    <name>FCCamTrax</name>
+    <message>
+      <source>  • ⚠ Arm too short, pressure angle may be too high</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source> seg</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>\n\nSee report view for details.</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Add</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Add at least one motion segment.</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Analysis Charts</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Analysis charts updated.</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Analysis failed:\n</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Arm Length:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Base Radius:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Blank Radius:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Bore Radius:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Cam Analysis</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Cam Angle (°)</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Cam generated successfully.</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Cam generation failed:\n</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Cam Type</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Chart panel failed to load. Check report view.</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Charts Unavailable</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Create Cam</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Create new cam design document</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Current:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Cylinder Radius:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Cylindrical Cam</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Default 4</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Delete</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Design Check</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Design Check...</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Design Warning</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Disk Cam</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Down</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Edit Segments...</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>End (°)</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>End Lift (mm)</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Error</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Follower</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Generate Cam</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Geometry</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Groove Depth:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Groove Width:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Grooved</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Initial Angle:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Linear Cam</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Motion</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Motion Law</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Motion Segments</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>New Cam Design</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>New cam design document created.</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Notice</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Offset:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Open cam parameter editor</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Oscillating</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Parameter Error</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Pivot X:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Pivot Y:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Points/deg:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Pos:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Pressure Angle</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Preview: disk/linear/cylindrical cams only</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Professional cam design workbench</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>pts/°</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Recommend arm, pivot, initial angle from base radius and max lift</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Recommendations:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Set motion segments first (max lift &gt; 0).</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Start (°)</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Start Lift (mm)</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Thickness:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Translating Off-center</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Translating On-center</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Type:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Unwrap Radius:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Up</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>• Design check not available for this follower type</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>• Reduce offset or increase base radius</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <source>Max swing angle</source>
+      <translation type="unfinished" />
+    </message>
+  </context>
+</TS>
